65cd578a969f0c049372bae9d6298160d580c4b4
Roselou <elizabethroselou@gmail.com>
2018-03-19 14:18:06 -0700

    the two questions gone over in class

--- functions.js
+++ functions.js
@@ -1,1 +1,20 @@
 // TODO: add your solutions here!
+
+// 3 BUILD YOUR OWN POWER FUNCTION
+function toTheNthPower(number, power) {
+var total = 1
+for (var i = 0; i < power; i++) {
+debugger
+total = number*total
+}
+return total
+}
+
+// 10 MAKE A TRIANGLE FUNCTION
+function printTriangle(length){
+var currentStr = ''
+for (var i = 0; i < length; i++) {
+currentStr += '*'
+console.log(currentStr)
+}
+}
