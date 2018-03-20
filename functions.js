a8f8536147c0d1317af7865ca4f68fb893eb97de
Roselou <elizabethroselou@gmail.com>
2018-03-19 22:16:22 -0700

    failed attempts to provide accurate solutions

--- functions.js
+++ functions.js
@@ -1,5 +1,31 @@
 // TODO: add your solutions here!
 
+
+//1. Build your own concatenation
+// Return a new string that is the combination of two arguments passed into the function
+// Example: dog and house will return doghouse
+
+var string1 = ''
+var string1 = ''
+function combineWords(string1, string2) {
+	losDos.concat(string1 + string2)
+}
+var result = combineWords('dog', 'house');
+console.log(result);
+
+//2 REPEAT A PHRASE
+//Display an argument phrase to the console n times
+
+var repeatedPhrase = ('');
+function repeatedPhrase (phrase, n){
+	var total = 1
+	turnstringinto number 
+	for (var i =0; i < power; i++) {
+		total = phrase * n 
+	}
+}
+
+
 // 3 BUILD YOUR OWN POWER FUNCTION
 function toTheNthPower(number, power) {
 var total = 1
@@ -9,6 +35,32 @@ total = number*total
 }
 return total
 }
+
+
+// 4 AREA OF CIRCLE: PIR^2
+function areaOfACircle(radius) {
+	let pi = 3.14
+	math.pow(pi*radius)
+}
+
+var result = areaOfACircle(2);
+console.log(result);
+// displays approximately 12.57
+
+// 5
+function pythagoreanTheorem(a, b) {
+var a = 1
+var b = 1
+total = 1
+for (var i = 0; i < power; i++) {
+debugger
+total = (a*total) + (b*total)
+}
+return total
+}
+
+var result = pythagoreanTheorem(3,4);
+console.log(result)
 
 // 10 MAKE A TRIANGLE FUNCTION
 function printTriangle(length){
